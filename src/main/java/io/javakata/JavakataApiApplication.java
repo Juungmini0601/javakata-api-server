f87e8d262b6720b97e4aed9742c3322f98cc60b1
김정민 <raon37910@gmail.com>
2025-03-22 18:38:09 +0900

    Initial Commit

---
+++ src/main/java/io/javakata/JavakataApiApplication.java
@@ -0,0 +1,13 @@
+package io.javakata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavakataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavakataApiApplication.class, args);
+	}
+
+}
