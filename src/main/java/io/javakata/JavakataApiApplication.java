f1a8a3174c60cf95f0330a412cc52ea0a3d4b7ce
김정민 <raon37910@gmail.com>
2025-03-23 14:37:05 +0900

    add: Redis, JWT Config

--- src/main/java/io/javakata/JavakataApiApplication.java
+++ src/main/java/io/javakata/JavakataApiApplication.java
@@ -2,7 +2,11 @@ package io.javakata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import io.javakata.common.config.JwtConfig;
+
+@EnableConfigurationProperties(value = JwtConfig.class)
 @SpringBootApplication
 public class JavakataApiApplication {
 
