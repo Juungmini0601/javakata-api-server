7ea783c03dfbc977f9dc9e9373dafc18349d68ce
김정민 <raon37910@gmail.com>
2025-03-23 10:10:53 +0900

    add test: UserController Register

--- src/test/java/io/javakata/fixtrue/FixtureUtil.java
+++ src/test/java/io/javakata/fixtrue/FixtureUtil.java
@@ -35,9 +35,9 @@ public class FixtureUtil {
 
 	public static User userFromCreateUserRequest(CreateUserRequest request) {
 		return beanValidationFixtureMonkey.giveMeBuilder(User.class)
-			.set("id", null)
+			.set("id", Arbitraries.longs().between(1L, 10L))
 			.set("email", request.email())
-			.set("password", request.password())
+			.set("password", Arbitraries.strings().withCharRange('a', 'z').ofMinLength(10).ofMaxLength(200))
 			.set("nickname", request.nickname())
 			.set("role", Role.ROLE_USER)
 			.set("oAuthProvider", OAuthProvider.LOCAL)
---
+++ src/test/java/io/javakata/unit/controller/user/UserControllerUnitTest.java
@@ -0,0 +1,95 @@
+package io.javakata.unit.controller.user;
+
+import static io.javakata.fixtrue.FixtureUtil.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.javakata.common.config.SecurityConfig;
+import io.javakata.common.error.ErrorType;
+import io.javakata.common.error.JavaKataException;
+import io.javakata.controller.ApiControllerAdvice;
+import io.javakata.controller.user.UserController;
+import io.javakata.controller.user.request.CreateUserRequest;
+import io.javakata.repository.user.Role;
+import io.javakata.repository.user.User;
+import io.javakata.service.user.UserService;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 23.
+ */
+@Import(SecurityConfig.class)
+@WebMvcTest({UserController.class, ApiControllerAdvice.class})
+public class UserControllerUnitTest {
+	// WebMvcTest는 의존성이 끊겨서 이렇게 사용해야함.
+	@MockitoBean
+	private UserService userService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Nested
+	@DisplayName("회원 생성 단위 테스트")
+	class CreateUserTest {
+
+		CreateUserRequest request;
+
+		@BeforeEach
+		void setUp() {
+			request = defaultCreateUserRequest();
+		}
+
+		@Test
+		@DisplayName("성공")
+		void success() throws Exception {
+			User user = userFromCreateUserRequest(request);
+			given(userService.register(any(CreateUserRequest.class))).willReturn(user);
+
+			mockMvc.perform(post("/api/v1/users")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request)))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.result").value("SUCCESS"))
+				.andExpect(jsonPath("$.data.id").isNumber())
+				.andExpect(jsonPath("$.data.nickname").value(request.nickname()))
+				.andExpect(jsonPath("$.data.email").value(request.email()))
+				.andExpect(jsonPath("$.data.role").value(Role.ROLE_USER.toString()))
+				.andExpect(jsonPath("$.data.password").doesNotExist());
+		}
+
+		@Test
+		@DisplayName("실패 - 중복된 이메일")
+		void fail_when_email_is_duplicated() throws Exception {
+			given(userService.register(any(CreateUserRequest.class)))
+				.willThrow(new JavaKataException(ErrorType.CONFLICT_ERROR, "duplicated email:" + request.email()));
+
+			mockMvc.perform(post("/api/v1/users")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request)))
+				.andDo(print())
+				.andExpect(status().isConflict())
+				.andExpect(jsonPath("$.result").value("ERROR"))
+				.andExpect(jsonPath("$.error.code").value(ErrorType.CONFLICT_ERROR.getCode().toString()));
+		}
+	}
+}
