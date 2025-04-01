a8f2c82e1965518342a24ebf046f4fa3b2708648
raon <raon37910@gmail.com>
2025-04-01 11:27:46 +0900

    Merge pull request #9
    * add: 회원탈퇴 API
    
    * test add: 회원탈퇴 API

--- src/main/java/io/javakata/repository/user/UserCommand.java
+++ src/main/java/io/javakata/repository/user/UserCommand.java
@@ -19,4 +19,9 @@ public class UserCommand {
 	public User save(User user) {
 		return userRepository.save(user);
 	}
+
+	@Transactional
+	public void delete(String email) {
+		userRepository.deleteByEmail(email);
+	}
 }
